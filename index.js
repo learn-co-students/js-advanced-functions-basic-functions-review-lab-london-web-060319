e436b689328a8e8ec494a808e14898f73aa052ee
Le H Ngo <seiji@o2.pl>
2019-07-18 19:29:56 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+function saturdayFun(arg = "roller-skate") {
+    return `This Saturday, I want to ${arg}!`
+}
+
+function mondayWork(arg = "go to the office") {
+    return `This Monday, I will ${arg}.`
+}
+
+function wrapAdjective(inni = "*" ) {
+    return function(text) {
+        return `You are ${inni}${text}${inni}!`
+    }
+}
+
+let Calculator = {
+    add: (n1, n2) => {
+        return n1 + n2
+    },
+    subtract: (n1, n2) => {
+        return n1 - n2
+    },
+    multiply: (n1, n2) => {
+        return n1 * n2
+    },
+    divide: (n1, n2) => {
+        return n1 / n2
+    }
+}
+
+function actionApplyer(integer, array) {
+    if (array.length === 0) {
+        return integer
+    } else {
+        let result = integer
+        array.forEach(item => {
+            result = item(result)
+        })
+        return result
+    }
+}
